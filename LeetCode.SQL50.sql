ad35751cc563bb35663736c68971b5d72c891cc2
priya-git31 <haripriyagouddr@gmail.com>
2024-11-26 12:28:14 -0500

    Leet Code 50

---
+++ LeetCode.SQL50.sql
@@ -0,0 +1,8 @@
+
+-- 1. Write a solution to find the ids of products that are both low fat and recyclable.
+SELECT 
+      product_id
+FROM 
+    Products
+WHERE
+     low_fats = 'Y' AND recyclable = 'Y'
